4b72ac9e572658a9ca856f6efe6e08294c276b06
Dmitrii1994 <grin-1994@mail.ru>
2017-12-05 01:07:41 +0200

    first alpha version ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  befor_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u|
+    devise_parameter_sanitizer.permit(:account_update) { |u|
       u.permit(:password, :password_confirmation, :current_password)
     }
+  end
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+
   end
 
 end
